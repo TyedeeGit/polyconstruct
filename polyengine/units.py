a09a363811a2cb58e8f780881cc3c519e226523a
TyedeeGit <79290235+TyedeeGit@users.noreply.github.com>
2023-12-16 14:14:38 -0800

    Fixed units again.

--- polyengine/units.py
+++ polyengine/units.py
@@ -4,6 +4,9 @@ from .metricutils import Unit, DefinedUnit, Measure
 # Conversion ratios
 METER_TO_FOOT = 0.3048
 KILOGRAM_TO_POUND = 0.45359237
+POUNDF_TO_NEWTON = 4.4482216152605
+PSI_TO_PASCAL = 6894.757293168
+FOOT_POUNDF_TO_JOULE = 1.355818
 
 # Base metric units
 meter = Unit('meters', 'length', symbol='m')
@@ -25,6 +28,6 @@ inch = DefinedUnit('inches', Measure(foot, 1 / 12), symbol='in')
 pound = DefinedUnit('pounds', Measure(kilogram, KILOGRAM_TO_POUND), symbol='lb')
 
 # Derived US units
-pound_force = DefinedUnit('pounds-force', (GRAVITY/METER_TO_FOOT) * pound * foot/(second**2), symbol='lbf')
-psi = DefinedUnit('pounds/in^2', 1 * pound_force/(inch**2), symbol='psi')
-foot_pound = DefinedUnit('foot-pounds', 1 * foot * pound_force, symbol='ft-lbf')
+pound_force = DefinedUnit('pounds-force', (GRAVITY/METER_TO_FOOT) * pound * foot/(second**2), POUNDF_TO_NEWTON*newton, symbol='lbf')
+psi = DefinedUnit('pounds/in^2', 1 * pound_force/(inch**2), PSI_TO_PASCAL*pascal, symbol='psi')
+foot_pound = DefinedUnit('foot-pounds', 1 * foot * pound_force, FOOT_POUNDF_TO_JOULE*joule, symbol='ft-lbf')
